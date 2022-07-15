e1045b0acb5f41e6b90fe6efffe3271aa401b071
KaivalyaVanguri <78576014+KaivalyaVanguri@users.noreply.github.com>
2022-07-15 21:38:53 +0530

    Add files via upload

--- AccountBalance.java
+++ AccountBalance.java
@@ -1,41 +1,29 @@
-class Balance 
-{
+package kaivalyaspkg;
+/* java kaivalyaspkg.AccountBalance is the command line to be used and not java AccountBalance
+ * As AccountBalanceis now part of the package kaivalyaspkg and can't be executed by itself.
+ * Remember you will need to be in the directory above the kaivalyaspkg when you execute this command.
+ */
+class Balance{
+	String name;
+	double bal;
 
-String name;
-double bal;
-
-Balance(){}
-Balance(Balance obj)
-{name=obj.name; bal=obj.bal;}
-Balance(String n, double b) 
-{
-name = n;
-bal = b;
+	Balance(String n, double b){
+		name = n;
+		bal = b;
+	}
+	void show(){
+		if (bal<0)
+			System.out.print("---> ");
+		System.out.println(name+": $"+bal);
+	}
 }
 
-void show() 
-{
-if(bal<0)
-System.out.print("--> ");
-System.out.println(name + ": $" + bal);
+class AccountBalance{
+	public static void main(String args[]){
+		Balance current[] = new Balance[3];
+		current[0] = new Balance("K. J. Fielding",123.23);
+		current[1] = new Balance("Will Tell",157.02);
+		current[2] = new Balance("Tom Jackson",-12.33);
+		for (int i = 0; i<3; i++) current[i].show();
+	}
 }
-
-}
-
-class AccountBalance 
-{
-public static void main(String args[]) 
-{
-Balance b1 = new Balance("K. J. Fielding", 123.23);
-Balance b2 = new Balance("Will Tell", 157.02);
-Balance b3 = new Balance(b2);
-b1.show();
-b2.show();
-b3.show();
-b3.bal=200;
-b2.show();
-b3.show();
-
-}
-}
-
